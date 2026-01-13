d7d6141a14db61c6cea4845d8c55949e0fe587e3
Div-404 <shivamdivakar407@gmail.com>
2026-01-13 16:17:09 +0530

    refactor: AsyncPipe + OnPush, remove manual subscriptions

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,33 +1,37 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { JsonDataService } from '../../core/services/json-data.service';
 import { User } from '../../shared/models/user';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports:[CommonModule],
+  imports:[CommonModule, ],
   templateUrl: './login.html',
-  styleUrls: ['./login.scss']
+  styleUrls: ['./login.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Login implements OnDestroy {
-  users: User[] = [];
-  private sub = new Subscription();
-
+export class Login implements OnInit {
+  // users: User[] = [];
+  // private sub = new Subscription();
   constructor(private service: JsonDataService) {
-    this.sub.add(
-      this.service.getUsers().subscribe({
-        next: (data) => {
-          this.users = data;
-          console.log('[Login] users', data);
-        },
-        error: (err) => console.error(err)
-      })
-    );
+    // this.sub.add(
+      //   this.service.getUsers().subscribe({
+    //     next: (data) => {
+    //       this.users = data;
+    //       console.log('[Login] users', data);
+    //     },
+    //     error: (err) => console.error(err)
+    //   })
+    // );
   }
+  users$!: Observable<User[]>;
+ngOnInit(): void {
+  this.users$ = this.service.getUsers();
+}
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.sub.unsubscribe();
+  // }
 }
