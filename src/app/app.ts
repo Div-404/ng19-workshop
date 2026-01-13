c5e53f9144d2ea3750352bd8c108f3b54f029ea0
Div-404 <shivamdivakar407@gmail.com>
2026-01-13 15:49:12 +0530

    feat: signal counter demo in AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  // styleUrl: './app.scss'
+  styleUrls: ['./app.scss'] 
 })
-export class App {
+export class App implements OnInit{
   protected title = 'ng19-workshop';
+  count = signal(0);
+  double = computed(()=> this.count()*2);
+  constructor(){
+    effect(()=>{
+      console.log("Effect count = ", this.count());
+    })
+  }
+  ngOnInit(){
+    setTimeout(()=> this.increment(),200);
+  }
+  increment(){
+    this.count.update(v=>v+1);
+    console.log("Increment", this.count());
+  }
 }
