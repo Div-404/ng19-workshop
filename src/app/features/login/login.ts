2d712fd8737c82c24cded6efc770da52a4066e36
Div-404 <shivamdivakar407@gmail.com>
2026-01-13 16:03:05 +0530

    feat: HttpClient + manual subscribe in LoginComponent

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { JsonDataService } from '../../core/services/json-data.service';
+import { User } from '../../shared/models/user';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports:[CommonModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrls: ['./login.scss']
 })
-export class Login {
+export class Login implements OnDestroy {
+  users: User[] = [];
+  private sub = new Subscription();
 
+  constructor(private service: JsonDataService) {
+    this.sub.add(
+      this.service.getUsers().subscribe({
+        next: (data) => {
+          this.users = data;
+          console.log('[Login] users', data);
+        },
+        error: (err) => console.error(err)
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 }
---
+++ src/app/shared/models/user.ts
@@ -0,0 +1,5 @@
+export interface User {
+  id: number;
+  email: string;
+  password: string;   // plain text for demo only
+}
