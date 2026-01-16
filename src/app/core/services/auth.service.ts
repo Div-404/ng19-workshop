bdb6895ff4b52dd3daaa8d511fa4bfede83adccb
Div-404 <shivamdivakar407@gmail.com>
2026-01-16 12:48:21 +0530

    feat: AuthService with signals, fake JWT, localStorage hydrate

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, signal } from "@angular/core";
+import { User } from "../../shared/models/user";
+import { HttpClient } from "@angular/common/http";
+import { Session } from "../../shared/models/session";
+import { catchError, map, of, tap } from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class AuthService{
+    private readonly api = 'http://localhost:3000';
+
+    // -------------signals------------
+    isLoggedIn = signal(false);
+    currentUser = signal<User|null>(null);
+
+    constructor(private http:HttpClient){
+        this.init();
+    }
+
+    private init(){
+        const raw =  localStorage.getItem('session');
+        if(raw){
+            try{
+                const session:Session = JSON.parse(raw);
+                if(new Date(session.expires)> new Date()){
+                    this.http.get<User>(`${this.api}/users/${session.userId}`)
+                    .subscribe(user=>{
+                        this.isLoggedIn.set(true);
+                        this.currentUser.set(user);
+                    });
+                } else{
+                    this.logout();
+                }
+            }
+            catch{
+                    this.logout();
+
+            }
+        }
+    }
+    logout(){
+        localStorage.removeItem('session');
+        this.isLoggedIn.set(false);
+        this.currentUser.set(null);
+    }
+    login(email:string, password:string){
+        return this.http.get<User[]>(`${this.api}/users?email=${email}&password=${password}`)
+        .pipe(
+            map(users => users[0]),
+            tap(user =>{
+                if(!user) throw new Error('Invalid Credentials');
+                const session: Session = {
+                    userId: user.id!,
+                    token: this.fakeJwt(),
+                    expires: new Date(Date.now() + 24*60*60*1000).toISOString()
+                };
+                localStorage.setItem('session', JSON.stringify(session));
+                this.isLoggedIn.set(true);
+                this.currentUser.set(user);
+            }),
+            map(()=> true),
+            catchError(()=>{
+                this.logout();
+                return of(false);
+            })
+        )
+    }
+
+    fakeJwt(){
+        return 'fake-jwt-' + Math.random().toString(36).slice(2);
+    }
+}
--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnDestroy, OnInit } from '@angular/core';
 import { JsonDataService } from '../../core/services/json-data.service';
 import { User } from '../../shared/models/user';
 import { Observable, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { LoginForm } from '../login-form/login-form';
+import { LoginForm, LoginPayload } from '../login-form/login-form';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,39 +16,55 @@ import { LoginForm } from '../login-form/login-form';
   styleUrls: ['./login.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Login implements OnInit {
+export class Login {
   // users: User[] = [];
-  users:any;
-  loading:any;
-  users$!: Observable<User[]>;
+//   users:any;
+//   loading:any;
+//   users$!: Observable<User[]>;
 
-  // private sub = new Subscription();
-  constructor(private service: JsonDataService) {
-    // this.sub.add(
-      //   this.service.getUsers().subscribe({
-    //     next: (data) => {
-    //       this.users = data;
-    //       console.log('[Login] users', data);
-    //     },
-    //     error: (err) => console.error(err)
-    //   })
-    // );
-     this.users = toSignal(this.service.getUsers(),{initialValue:[]})
-  this.loading = computed(()=> this.users.length == 0)
-  }
-
-ngOnInit(): void {
-//   // this.users$ = this.service.getUsers();
-//   this.users = toSignal(this.service.getUsers(),{initialValue:[]})
+//   // private sub = new Subscription();
+//   constructor(private service: JsonDataService,
+    
+//   ) {
+//     // this.sub.add(
+//       //   this.service.getUsers().subscribe({
+//     //     next: (data) => {
+//     //       this.users = data;
+//     //       console.log('[Login] users', data);
+//     //     },
+//     //     error: (err) => console.error(err)
+//     //   })
+//     // );
+//      this.users = toSignal(this.service.getUsers(),{initialValue:[]})
 //   this.loading = computed(()=> this.users.length == 0)
-}
+//   }
 
-onLogin(payload: any): void {
-    console.log('[LoginPage] payload', payload);
-    // TODO: authenticate in next segments
-  }
+// ngOnInit(): void {
+// //   // this.users$ = this.service.getUsers();
+// //   this.users = toSignal(this.service.getUsers(),{initialValue:[]})
+// //   this.loading = computed(()=> this.users.length == 0)
+// }
+
+// onLogin(payload: any): void {
+//     console.log('[LoginPage] payload', payload);
+//     // TODO: authenticate in next segments
+//   }
   // ngOnDestroy(): void {
   //   this.sub.unsubscribe();
   // }
+    public auth = inject(AuthService);
+  private router = inject(Router);
+
+  users = toSignal(inject(JsonDataService).getUsers(), { initialValue: [] });
+
+  onLogin(payload: LoginPayload): void {
+    this.auth.login(payload.email, payload.password).subscribe(success => {
+      if (success) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Invalid credentials');
+      }
+    });
+  }
   
 }
